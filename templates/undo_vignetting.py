c31843b62936a664e2c0c1a441fbabb42f539793
Maxwell Zheng <zhengmaxwell@gmail.com>
2022-10-17 13:51:04 -0400

    Update undo_vignetting.py

--- templates/undo_vignetting.py
+++ templates/undo_vignetting.py
@@ -1,17 +1,74 @@
-# Add other imports from available listing as your see fit.
-import sys
+import cv2
 import numpy as np
-from cv2 import imread, imwrite, resize, imshow, waitKey, destroyAllWindows
+import sys
 
-# You can pass additional arguments here by modifying the function signature,
-# as long as each additional argument has a default value.
-def undo_vignetting(I):
-    # Load images
-    I = imread(I)
+def undo_vignetting(img, sigma, brightness):
 
-    # Return corrected image.
-    return Icorrect
+    """
+    Parameters
+    -----------
+    img: str
+        image path
+    sigma: float
+        standard deviation for gaussian kernel as a funtion of dimension
+        both dimensions use the same sigma
+        default is half the the dimension
+    brightness: float
+        increased brightness after vignette correction
+        (brightness * 100)% image brightness
+        default is 1.3
+    """
+
+    sigma = sigma or 0.5
+    brightness = brightness or 1.3
+
+    img = cv2.imread(img)
+    h, w = img.shape[:2]
+    og_img = img.copy()
+
+    # get gaussian mask
+    sigma_w = w * sigma
+    sigma_h = h * sigma
+    kernel_x = cv2.getGaussianKernel(w, sigma_w)
+    kernel_y = cv2.getGaussianKernel(h, sigma_h)
+    kernel = kernel_y * kernel_x.T
+    mask = 255 * kernel / np.linalg.norm(kernel)
+
+    # apply mask to 3 colour channels
+    for i in range(3):
+        img[:,:,i] = normalize(img[:,:,i] / mask, 255)
+
+    # increase brightness
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = np.array(img, dtype = np.float64)
+    for i in range(1, 3):
+        img[:,:,i] = img[:,:,i] * brightness
+        img[:,:,i][img[:,:,i] > 255]  = 255
+    img = np.array(img, dtype = np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_HSV2BGR)
+
+    cv2.imshow("og", og_img)
+    cv2.imshow("fixed", img)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    return img
+
+
+# normalize values between 0 and 255 for 8bit images
+def normalize(img, maxval):
+
+    return ((img - img.min()) * (1 / (img.max() - img.min()) * maxval)).astype("uint8")
 
 if __name__ == "__main__":
-    # Add test code here if you desire.
-    pass
+
+    img_file = sys.argv[1]
+    sigma = None
+    brightness = None
+    if len(sys.argv) > 2:
+        sigma = float(sys.argv[2])
+        if len(sys.argv) > 3:
+            brightness = float(sys.argv[3])
+
+    undo_vignetting(img_file, sigma, brightness)
